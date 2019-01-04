5dc7e1331f4412baa3c07b3ecc4c0b23a1031cdb
Brad Niedt <bradleyniedt@gmail.com>
2019-01-04 09:12:41 -0700

    Adds small change to discount model test.

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Discount, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:discount_type) }
-    it { should validate_presence_of(:amount).only_integer }
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount).only_integer }
     it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
   end
   describe 'relationships' do
