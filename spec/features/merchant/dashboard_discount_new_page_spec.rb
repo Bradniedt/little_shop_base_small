58f21d0e88fe201d6bfd492b9b2e818639676cd4
Brad Niedt <bradleyniedt@gmail.com>
2019-01-05 10:14:12 -0700

    Added tests for new discount creation.

---
+++ spec/features/merchant/dashboard_discount_new_page_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+describe 'as a merchant user' do
+  context 'when I visit my dashboard discounts index page and I click on create a new discount' do
+    it 'takes me to a form to create a new discount, and I can create a new discount' do
+      merch = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
+      visit dashboard_discounts_path
+
+      click_on('Create A New Discount')
+      expect(current_path).to eq(new_dashboard_discount_path)
+
+      fill_in :discount_discount_type, with: 0
+      fill_in :discount_amount, with: 5
+      fill_in :discount_quantity, with: 10
+      click_on("Create Discount")
+
+      discount = Discount.all.last
+
+      expect(current_path).to eq(dashboard_discounts_path)
+      within("#discount-#{discount.id}") do
+        expect(page).to have_content(discount.id)
+        expect(page).to have_content(discount.discount_type)
+        expect(page).to have_content(discount.amount)
+        expect(page).to have_content(discount.quantity)
+        expect(page).to have_link('Edit This Discount')
+        expect(page).to have_link('Delete This Discount')
+      end
+    end
+    it 'takes me to a form to create a new discount, and it wont let me create a discount without all fields filled' do
+      merch = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
+      visit dashboard_discounts_path
+
+      click_on('Create A New Discount')
+      expect(current_path).to eq(new_dashboard_discount_path)
+
+      click_on("Create Discount")
+
+      discount = Discount.all.last
+
+      expect(current_path).to eq(new_dashboard_discount_path)
+      expect(page).to have_content("Discount type cannot be blank")
+      expect(page).to have_content("Discount amount cannot be blank")
+      expect(page).to have_content("Discount quantity cannot be blank")
+    end
+    it 'takes me to a form to create a new discount, and I can only create a discount with the same type as my other discounts' do
+      merch = create(:merchant)
+      discount_1 = merch.discounts.create(discount_type: 0, amount: 5, quantity: 10)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
+      visit dashboard_discounts_path
+
+      click_on('Create A New Discount')
+      expect(current_path).to eq(new_dashboard_discount_path)
+
+      fill_in :discount_discount_type, with: 1
+      fill_in :discount_amount, with: 10
+      fill_in :discount_quantity, with: 20
+      click_on("Create Discount")
+
+      discount = Discount.all.last
+
+      expect(current_path).to eq(new_dashboard_discount_path)
+      expect(page).to have_content("Discount type must match existing discounts")
+    end
+  end
+end
