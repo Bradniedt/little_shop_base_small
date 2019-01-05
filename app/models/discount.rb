da26131839e8388eee1c5f50fb7a7bbf5ea88c18
Brad Niedt <bradleyniedt@gmail.com>
2019-01-05 11:51:15 -0700

    Adds validation for discount. Discount model tests passing.

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,6 +3,9 @@ class Discount < ApplicationRecord
   validates :amount, presence: true, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0 }
 
   belongs_to :user
 end
