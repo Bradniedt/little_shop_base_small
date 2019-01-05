98e8c73c323cd7cbddaf8d081993a7dfefc71089
Brad Niedt <bradleyniedt@gmail.com>
2019-01-05 12:24:37 -0700

    Adds model and discount changes to pass first two discount create tests.

--- app/controllers/dashboard/discounts_controller.rb
+++ app/controllers/dashboard/discounts_controller.rb
@@ -2,4 +2,46 @@ class Dashboard::DiscountsController < Dashboard::BaseController
   def index
     @discounts = current_user.discounts
   end
+
+  def new
+    @discount = Discount.new
+    @form_path = [:dashboard, @discount]
+  end
+
+  def create
+    dp = discount_params
+    @merchant = current_user
+    if @merchant.discounts == []
+      @discount = @merchant.discounts.create(dp)
+      if @discount.save
+        flash[:success] = "Discount #{@discount.id} has been created!"
+        redirect_to dashboard_discounts_path
+      else
+        @form_path = [:dashboard, @discount]
+        render :new
+      end
+    else
+      if @merchant.discounts.first.discount_type == dp[:discount_type]
+        @discount = @merchant.discounts.create(dp)
+        if @discount.save
+          flash[:success] = "Discount #{@discount.id} has been created!"
+          redirect_to dashboard_discounts_path
+        else
+          @form_path = [:dashboard, @discount]
+          render :new
+        end
+      else
+        @discount = @merchant.discounts.create(dp)
+        flash[:notice] = "Discount type must match existing discounts"
+        @form_path = [:dashboard, @discount]
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:discount_type, :amount, :quantity)
+  end
 end
--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,11 +1,13 @@
 class Discount < ApplicationRecord
-  validates_presence_of :discount_type
+  validates :discount_type, presence: true, allow_blank: false
   validates :amount, presence: true, numericality: {
     only_integer: true,
-    greater_than_or_equal_to: 0 }
+    greater_than_or_equal_to: 0,
+    allow_blank: false}
   validates :quantity, presence: true, numericality: {
     only_integer: true,
-    greater_than_or_equal_to: 0 }
+    greater_than_or_equal_to: 0,
+    allow_blank: false }
 
   belongs_to :user
 end
