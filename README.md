8425a1a7702e3431892a9e06771f78206a41c224
Brad Niedt <bradleyniedt@gmail.com>
2019-01-08 15:44:02 -0700

    Adds Readme and seed data changes.

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
-# README
+# Little Shop
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Summary
 
-Things you may want to cover:
+Little Shop is an e-commerce web application that has visitor, user, merchant, and admin authorization levels. Merchants can individually fulfill separate items in aggregate orders that users place with the store. Visitors can browse, add items to carts, and see statistics data, but cannot checkout without registering or logging in. Admins have access to almost all functionality that
+users and merchants do, and they can also enable and disable users and merchants.
 
-* Ruby version
+## Setup
 
-* System dependencies
+If you would like to run this app yourself, this repo can be forked and then cloned by clicking on the "clone or download" button, then following cloning instructions. After forking and cloning, run the following commands in your terminal while in the project directory:
 
-* Configuration
+-- `bundle install`
 
-* Database creation
+-- `rake db:{drop,create,migrate,seed}'``
 
-* Database initialization
+-- `rails s`
 
-* How to run the test suite
+## Prerequisites
 
-* Services (job queues, cache servers, search engines, etc.)
+Rails 5.1
+A terminal
+Github Account
 
-* Deployment instructions
+## How To Run The Test Suite
 
-* ...
+To run the test suite, simply run the following command in your terminal:
+
+`rspec`
+
+If you'd like to run a specific test or directory of tests, you can run commands similar to the following:
+
+`rspec spec/models/` this will run all model tests
+`rspec spec/models/item_spec.rb` this will run all item model tests
+`rspec spec/models/item_spec.rb:31` this will run a specific item model test.
+
+## Contributors
+Ian Douglas wrote the vast majority of this app, while Bradley Niedt added slugs to users and items, as well as bulk discounts.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@ OrderItem.destroy_all
 Order.destroy_all
 Item.destroy_all
 User.destroy_all
+Discount.destroy_all
 
 admin = create(:admin)
 user = create(:user)
 merchant_1 = create(:merchant, email: "email_1@aol.com", password: "11111")
 
 merchant_2, merchant_3, merchant_4 = create_list(:merchant, 3)
+
+discount_1 = merchant_1.discounts.create(discount_type: 0, amount: 5, quantity: 10)
+discount_2 = merchant_1.discounts.create(discount_type: 0, amount: 10, quantity: 20)
+discount_3 = merchant_2.discounts.create(discount_type: 0, amount: 5, quantity: 10)
+discount_4 = merchant_2.discounts.create(discount_type: 0, amount: 10, quantity: 20)
+discount_5 = merchant_3.discounts.create(discount_type: 1, amount: 5, quantity: 10)
+discount_6 = merchant_3.discounts.create(discount_type: 1, amount: 10, quantity: 25)
+discount_7 = merchant_4.discounts.create(discount_type: 1, amount: 5, quantity: 10)
+discount_8 = merchant_4.discounts.create(discount_type: 1, amount: 10, quantity: 30)
 
 inactive_merchant_1 = create(:inactive_merchant)
 inactive_user_1 = create(:inactive_user)
@@ -29,10 +39,10 @@ Random.new_seed
 rng = Random.new
 
 order = create(:completed_order, user: user)
-create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: rng.rand(3).days.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: rng.rand(5).days.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 11, created_at: rng.rand(3).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 7, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 26, created_at: rng.rand(5).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 13, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 
 order = create(:order, user: user)
 create(:order_item, order: order, item: item_1, price: 1, quantity: 1)
